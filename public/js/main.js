0c1e059faaf19908593856c5c1cfe4c37aaf8b02
jennvance <vancejenniferm@gmail.com>
2018-03-09 15:36:21 -0700

    new binary function

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,19 @@ function checkTime(i){
 	}
 }
 
+
 function startTime(){
-	
+	var currentTime = new Date()
+	//get time; make it human readable
+	var hours = checkTime(currentTime.getHours())
+	var minutes = checkTime(currentTime.getMinutes())
+	var seconds = checkTime(currentTime.getSeconds())
+	console.log(hours + ":" + minutes + ":" + seconds)
+	//get binary time
+	var hoursBi = currentTime.getHours().toString(2)
+	var minutesBi = currentTime.getMinutes().toString(2);
+	var secondsBi = currentTime.getSeconds().toString(2);
+	//ensure all numbers are 6 digits
+	//.....
+
 }
